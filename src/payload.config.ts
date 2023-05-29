fd435264b4a13623bd4aa706525141033942f258
mnmustafa1109 <mnmustafa1109@gmail.com>
2023-05-29 09:43:30 +0500

    added server url of fly.dev

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,7 +7,7 @@ import Users from './collections/Users';
 import Media from './collections/Media';
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  serverURL: 'https://logn-backend.fly.dev/',
   admin: {
     user: Users.slug,
   },
